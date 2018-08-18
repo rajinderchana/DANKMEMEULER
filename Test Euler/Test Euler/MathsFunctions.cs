d4022a7f4f1345621838992ee1c299bad003c874
Jamie Mair <jamie.mair@hotmail.co.uk>
2018-08-18 23:28:32 +0100

    Added isprime and mathsfunctions

---
+++ Test Euler/Test Euler/MathsFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Euler
+{
+    public class MathsFunctions
+    {
+
+        /// <summary>
+        /// Checks whether an input function is prime or not.
+        /// </summary>
+        /// <param name="n">The number to check.</param>
+        public bool IsPrime(int n)
+        { 
+            for (int i = 2; i < n; i++)
+            {
+                if (n % 2 == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+    }
+}
