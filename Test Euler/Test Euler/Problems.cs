dad4ac2283ab8999961eebf8fe4f2f93be521b0c
rajinderchana <rajinder_c@outlook.com>
2018-08-18 23:19:28 +0100

    Added comments

--- Test Euler/Test Euler/Problems.cs
+++ Test Euler/Test Euler/Problems.cs
@@ -8,17 +8,21 @@ namespace Test_Euler
 {
     static class Problems
     {
+        /// <summary>
+        /// Sums all multiples of 3 or 5 below n
+        /// </summary>
+        /// <param name="n">Upper bound of sum</param>
+        /// <returns>Retuns total sum</returns>
         public static int Problem1(int n = 1000)
         {
             int sum = 0;
             for (int i = 0; i < n; i++)
             {
-                if (i % 3==0 || i % 5==0) 
+                if (i % 3 == 0 || i % 5 == 0)
                 {
                     sum = sum + i;
                 }
             }
-
             return sum;
         }  
 
