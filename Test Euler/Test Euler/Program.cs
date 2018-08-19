00d0f1c38b6ead8d85408fb420d3e339d5bfe14e
rajinderchana <rajinder_c@outlook.com>
2018-08-19 16:59:55 +0100

    Finished problem 2

--- Test Euler/Test Euler/Program.cs
+++ Test Euler/Test Euler/Program.cs
@@ -13,6 +13,9 @@ namespace Test_Euler
             int prob1ans = Problems.Problem1(n:1000);
             Console.WriteLine($"The answer to Problem 1 is {prob1ans}");
 
+            int prob2ans = Problems.Problem2();
+
+            Console.WriteLine($"The answer to Problem 2 is {prob2ans}");
             Console.ReadKey();
         }
     }
