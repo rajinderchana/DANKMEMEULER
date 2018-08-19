7f6981b8ce3a26bbcc6fa49fb7f052c751c1cf28
rajinderchana <rajinder_c@outlook.com>
2018-08-19 16:36:41 +0100

    dank

--- Test Euler/Test Euler/Program.cs
+++ Test Euler/Test Euler/Program.cs
@@ -10,11 +10,8 @@ namespace Test_Euler
     {
         static void Main(string[] args)
         {
-            int prob1ans = Problems.Problem1(n: 1000);
+            int prob1ans = Problems.Problem1(n:1000);
             Console.WriteLine($"The answer to Problem 1 is {prob1ans}");
-
-            int prob2ans = Problems.Problem2();
-            Console.WriteLine($"The answer to Problem 2 is {prob2ans}");
 
             Console.ReadKey();
         }
